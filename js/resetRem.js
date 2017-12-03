e11e972274cb97bf8592d72fb5137b49e2b9874e
ilofxyt123 <455171468@qq.com>
2017-12-03 16:17:40 +0800

    12.3/16.17

--- js/resetRem.js
+++ js/resetRem.js
@@ -1,16 +1,30 @@
 /**
  * Created by Z on 2017/6/21.
  */
-(function(){
-    var setSize = function(){
+( function() {
+
+    var setSize = function() {
+
+        var standard = 640;
+
         var _width = window.innerWidth;
-        var size = (_width/640)*100;
-        if(size<50){size=50;}
-        if(size>100){size=100;}
+
+        if( _width >= 640 ){
+            standard = 1136
+        }else{
+            standard = 640
+        }
+        var size = ( _width / standard ) * 100;
         document.documentElement.style.fontSize = size + 'px';
+
     };
+
     setSize();
-    window.onresize = function(){
-//                setSize();
-    };
-})();
+
+    window.addEventListener( "resize", function(){
+
+        setSize();
+
+    } )
+
+} )();
